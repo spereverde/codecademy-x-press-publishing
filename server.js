f81aa863a32c6eea4f09b7b48134201072c0a1ac
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 09:56:16 +0100

    add server.js and deps

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+const app = express();
+
