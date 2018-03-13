88da46091117a04860caa670d47cb6b96d8b49a5
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 15:48:08 +0100

    add post route for series

--- api/series.js
+++ api/series.js
@@ -31,4 +31,28 @@ seriesRouter.get('/:seriesId', (req, res, next) => {
     res.status(200).json({series: req.series});
 });
 
+const validateSeries = (req, res, next) => {
+    req.name = req.body.series.name;
+    req.description = req.body.series.description;
+    if (!req.name || !req.description) {
+        return res.sendStatus(400);
+    } else {
+        next();
+    }
+};
+
+
+seriesRouter.post('/', validateSeries, (req, res, next) => {
+    db.run(`INSERT INTO Series (name, description) VALUES("${req.name}", "${req.description}")`,
+        function (error) {
+            if (error) {
+                next(error);
+            } else {
+                db.get(`SELECT * FROM Series WHERE id = ${this.lastID}`, (error, series) => {
+                    res.status(201).json({series: series});
+                });
+            }
+        });
+});
+
 module.exports = seriesRouter;
