1482da5cdb9147fcc96eec9638053696b7cfaecd
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 10:04:10 +0100

    setup babel

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const morgan = require('morgan');
 const errorhandler = require('errorhandler');
 const app = express();
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+
+module.exports = app;
