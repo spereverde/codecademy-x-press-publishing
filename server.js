46c8b244428bf2f494d6a703348be7e5aa6dceff
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 10:29:22 +0100

    use the middleware

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const morgan = require('morgan');
 const errorhandler = require('errorhandler');
 const app = express();
 
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(errorhandler());
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-})
-
+});
 
 module.exports = app;
