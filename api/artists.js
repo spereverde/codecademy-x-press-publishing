731d9f863877cfddaadc6d0b863fc5beda41aa0f
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 12:25:15 +0100

    add artistId param and route

--- api/artists.js
+++ api/artists.js
@@ -4,4 +4,35 @@ const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+artistsRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Artist WHERE is_currently_employed = 1`, (err, artists) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({artists: artists});
+        }
+    });
+});
+
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+    db.get(`SELECT * FROM Artist WHERE id = $artistId;`,
+        { $artistId: artistId },
+        (error, artist) => {
+            if (error) {
+                next(error);
+            } else if (artist) {
+                req.artist = artist;
+                next();
+            } else {
+                res.status(404).send(`Artist with that name does not exist`);
+                //res.sendStatus(404);
+            }
+        });
+});
+
+artistsRouter.get('/:artistId', (req, res, next) => {
+    res.status(200).json({artist: req.artist});
+    next();
+});
+
 module.exports = artistsRouter;
